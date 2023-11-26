fe690e68dc18a33e03de1ed3b8c18509a0ed8727
Rak65 <rakeshp6505@gmail.com>
2023-11-27 00:50:20 +0530

    [Added] : Find the cheapest hotel for given date range for a regular customer using java streams.

--- hotelreservationsystem/HotelReservationSystem.java
+++ hotelreservationsystem/HotelReservationSystem.java
@@ -19,26 +19,26 @@ public class HotelReservationSystem {
             LocalDate endDate = LocalDate.of(2020, 9, 12);
 
             validateDateRange(startDate, endDate);
-//        Hotel cheapestHotelRegular = reservationSystem.findCheapestHotel(startDate, endDate, false);
+        Hotel cheapestHotelRegular = reservationSystem.findCheapestHotel(startDate, endDate, false);
 //        Hotel bestHotelRegular = reservationSystem.findBestHotel(startDate, endDate, false);
-//
-//        System.out.println("Cheapest Hotel (Regular Customer): " + cheapestHotelRegular.getName() +
-//                ", Total Rates: $" + cheapestHotelRegular.calculateTotalRate(startDate, endDate, false) +
-//                ", Rating: " + cheapestHotelRegular.getRating());
-//
+
+        System.out.println("Cheapest Hotel (Regular Customer): " + cheapestHotelRegular.getName() +
+                ", Total Rates: $" + cheapestHotelRegular.calculateTotalRate(startDate, endDate, false) +
+                ", Rating: " + cheapestHotelRegular.getRating());
+
 //        System.out.println("Best Hotel (Regular Customer): " + bestHotelRegular.getName() +
 //                ", Total Rates: $" + bestHotelRegular.calculateTotalRate(startDate, endDate, false) +
 //                ", Rating: " + bestHotelRegular.getRating());
-            Hotel cheapestHotelRewards = reservationSystem.findCheapestHotel(startDate, endDate, true);
-            Hotel bestHotelRewards = reservationSystem.findBestHotel(startDate, endDate, true);
-
-            System.out.println("Cheapest Hotel (Rewards Customer): " + cheapestHotelRewards.getName() +
-                    ", Total Rates: $" + cheapestHotelRewards.calculateTotalRate(startDate, endDate, true) +
-                    ", Rating: " + cheapestHotelRewards.getRating());
-
-            System.out.println("Best Hotel (Rewards Customer): " + bestHotelRewards.getName() +
-                    ", Total Rates: $" + bestHotelRewards.calculateTotalRate(startDate, endDate, true) +
-                    ", Rating: " + bestHotelRewards.getRating());
+//            Hotel cheapestHotelRewards = reservationSystem.findCheapestHotel(startDate, endDate, true);
+//            Hotel bestHotelRewards = reservationSystem.findBestHotel(startDate, endDate, true);
+//
+//            System.out.println("Cheapest Hotel (Rewards Customer): " + cheapestHotelRewards.getName() +
+//                    ", Total Rates: $" + cheapestHotelRewards.calculateTotalRate(startDate, endDate, true) +
+//                    ", Rating: " + cheapestHotelRewards.getRating());
+//
+//            System.out.println("Best Hotel (Rewards Customer): " + bestHotelRewards.getName() +
+//                    ", Total Rates: $" + bestHotelRewards.calculateTotalRate(startDate, endDate, true) +
+//                    ", Rating: " + bestHotelRewards.getRating());
         }catch (IllegalArgumentException e) {
             System.err.println("Invalid date range: " + e.getMessage());
         }
